78e0bca2c44ba503331b8076ef7979ff5391c418
malkebu-lan <malkebu-lan@uequations.com>
2020-05-06 18:38:37 -0400

    initial commit

---
+++ src/main/java/com/uequations/Solution.java
@@ -0,0 +1,33 @@
+package com.uequations;
+
+import com.uequations.compute.UsernameValidator;
+
+//  Sample data:
+//      Julia
+//      Samantha
+//      Samantha_21
+//      1Samantha
+//      Samantha?10_2A
+//      JuliaZ007
+//      Julia@007
+//      _Julia007
+public class Solution {
+
+
+    public static void main(String[] args) {
+        String username = "Julia";
+        System.out.println(solve(username));
+    }
+
+    public static boolean solve(String _username) {
+
+        if (_username.matches(UsernameValidator.regularExpression)) {
+            System.out.println("Valid");
+            return true;
+        } else {
+            System.out.println("Invalid");
+            return false;
+        }
+    }
+}
+
---
+++ src/main/java/com/uequations/compute/UsernameValidator.java
@@ -0,0 +1,5 @@
+package com.uequations.compute;
+
+public class UsernameValidator {
+    public static final String regularExpression = "[a-zA-Z]{1}[a-zA-Z0-9_]{7,29}";
+}
